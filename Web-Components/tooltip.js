b751e12fe750fa5bf441984ea1f7eb2845af7aae
Ryan <rrmhearts@gmail.com>
2020-04-07 12:52:59 -0400

    Using shadow dom and HTML template.

--- Web-Components/tooltip.js
+++ Web-Components/tooltip.js
@@ -3,33 +3,42 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = 'Some dummy tooltip text.';
+    /*
+      Isolate the dom of this Element from the real dom!
+      Do not affect our dom by changes to the global styles.
+    */
+    this.attachShadow({ mode: 'open' }); // access shadow dom tree
+
+    // HTML Template, we can access the NORMAL dom from the constructor
+    const template = document.querySelector('#tooltip-template');
+    this.shadowRoot.appendChild(template.content.cloneNode(true)) // deep clone template
   }
 
   connectedCallback() {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text');
     }
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    
+    // Property from attaching shadow dom.
+    this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    // Global styles are affecting our web component!
     this._tooltipContainer.style.backgroundColor = 'black';
     this._tooltipContainer.style.color = 'white';
     this._tooltipContainer.style.position = 'absolute';
     this._tooltipContainer.style.zIndex = '10';
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
