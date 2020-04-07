37eac21184edddff05310cc218e9e6a2c588d866
Ryan <rrmhearts@gmail.com>
2020-04-07 16:49:51 -0400

    stencil component

---
+++ Stencil-App-Starter/stencil-component/stencil.config.ts
@@ -0,0 +1,19 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-component',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
