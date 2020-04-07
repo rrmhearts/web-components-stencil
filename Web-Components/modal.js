46778b73f89bd7f628241059e9207eb435c9138a
Ryan <rrmhearts@gmail.com>
2020-04-07 16:35:33 -0400

    backdrop cancel. css transitions.

--- Web-Components/modal.js
+++ Web-Components/modal.js
@@ -23,9 +23,13 @@ class Modal extends HTMLElement {
                 pointer-events: all;
             }
 
+            :host([opened]) #modal {
+                top: 15vh;
+            }
+
             #modal {
                 position: fixed;
-                top: 15vh;
+                top: 10vh;
                 left: 25%;
                 width: 50%;
                 z-index: 100;
@@ -37,14 +41,17 @@ class Modal extends HTMLElement {
                 justify-content: space-between;
                 opacity: 0;
                 pointer-events: none;
+                transition: all 0.3s ease-out;
             }
 
             header {
                 padding: 1rem;
+                border-bottom: 1px solid #ccc;
             }
 
             ::slotted(h1) {
                 font-size: 1.25rem;
+                margin: 0;
             }
 
             #main {
@@ -80,10 +87,10 @@ class Modal extends HTMLElement {
     slots[1].addEventListener('slotchange', event => {
       console.dir(slots[1].assignedNodes());
     });
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
     const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
     const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
-
-    // on Cancel. Hide modal, dispatch "cancel" event! See _cancel
+    backdrop.addEventListener('click', this._cancel.bind(this));
     cancelButton.addEventListener('click', this._cancel.bind(this));
     confirmButton.addEventListener('click', this._confirm.bind(this));
     // cancelButton.addEventListener('cancel', () => {
