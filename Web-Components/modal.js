606a47f455e63ce559634c2defefa6b5325792cb
Ryan <rrmhearts@gmail.com>
2020-04-07 15:46:14 -0400

    open modal from button click. use opened attribute in css!

--- Web-Components/modal.js
+++ Web-Components/modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.isOpen = false;
     this.shadowRoot.innerHTML = `
         <style>
             #backdrop {
@@ -12,7 +13,16 @@ class Modal extends HTMLElement {
                 height: 100vh;
                 background: rgba(0,0,0,0.75);
                 z-index: 10;
+                opacity: 0;
+                pointer-events: none;
             }
+
+            :host([opened]) #backdrop,
+            :host([opened]) #modal {
+                opacity: 1;
+                pointer-events: all;
+            }
+
             #modal {
                 position: fixed;
                 top: 15vh;
@@ -25,6 +35,8 @@ class Modal extends HTMLElement {
                 display: flex;
                 flex-direction: column;
                 justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
             }
 
             header {
@@ -64,6 +76,27 @@ class Modal extends HTMLElement {
             </section>
         </div>
     `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true;
+      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+      // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+    } else {
+      this.isOpen = false;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['opened'];
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+    this.isOpen = true;
   }
 }
 
