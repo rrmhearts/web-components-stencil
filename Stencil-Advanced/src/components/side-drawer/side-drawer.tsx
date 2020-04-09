270f6557d2ef2990fefaa81a4a43066be2a66375
Ryan <rrmhearts@gmail.com>
2020-04-09 10:20:42 -0400

    animate side drawer. slots.

--- Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
+++ Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
@@ -2,19 +2,35 @@ import { Component, Prop } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
-  styleUrl: './side-drawer.css', // Automatically applied!
-  shadow: true // Scope css to this component's shadow dom.
+  styleUrl: './side-drawer.css',
+  shadow: true
 })
 export class SideDrawer {
-  // Prop watches title, if changes, calls render.
-  // reflectToAttr may have changed to reflect in future Stencil releases.
-  // ^-- updates attribute of html tag. In outside code wants to interact with attribute.
-  @Prop({reflectToAttr: true}) title: string;
+  @Prop({ reflectToAttr: true }) title: string;
+  //@Prop() open: boolean;
 
   render() {
+
+    // Instead of using condition, let's use CSS
+    // let content = null;
+    // if (this.open) {
+    //   content = (
+    //     <aside>
+    //       <header>
+    //         <h1>{this.title}</h1>
+    //       </header>
+    //       <main>
+    //         <slot />
+    //       </main>
+    //     </aside>
+    //   );
+    // }
+    // SLOT: things between the tags go into slot.
     return (
       <aside>
-        <header><h1>{this.title}</h1></header>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
         <main>
           <slot />
         </main>
