a6194023c81f19c4aa1837a6a73436fb43c051a9
Ryan <rrmhearts@gmail.com>
2020-04-09 10:43:21 -0400

    Props w/ attributes, mutability. Tabs w/ State.

--- Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
+++ Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
@@ -6,34 +6,67 @@ import { Component, Prop } from '@stencil/core';
   shadow: true
 })
 export class SideDrawer {
+
+  // Only Changes from INSIDE. 
+  @State() showContactInfo = false; // typescript infers type
+
   @Prop({ reflectToAttr: true }) title: string;
-  //@Prop() open: boolean;
+
+  // Stencil will synchronize everything better with a Prop. Attribute will match prop.
+  // Props are IMMUTABLE inside component. Data always flows on direction, outside to inside!,
+  // ^-- overwrite that by setting mutable flag. Now can internally change.
+  @Prop({ reflectToAttr: true, mutable: true }) open: boolean;
+
+  // Can be named anything. Event triggered could be "onEvent"
+  onCloseDrawer() {
+    this.open = false;
+  }
+
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact';
+  }
 
   render() {
+    let mainContent = <slot />; // Default is slot content.
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone: 49802384032</li>
+            <li>
+              E-Mail:
+              <a href="mailto:something@something.com">
+                something@something.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
 
-    // Instead of using condition, let's use CSS
-    // let content = null;
-    // if (this.open) {
-    //   content = (
-    //     <aside>
-    //       <header>
-    //         <h1>{this.title}</h1>
-    //       </header>
-    //       <main>
-    //         <slot />
-    //       </main>
-    //     </aside>
-    //   );
-    // }
-    // SLOT: things between the tags go into slot.
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this) /*refer to SD, not button*/}>X</button>
         </header>
-        <main>
-          <slot />
-        </main>
+        <section id="tabs">
+          <button
+            class={!this.showContactInfo ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'nav') /*nav is arg to be passed*/}
+          >
+            Navigation
+          </button>
+          <button
+            class={this.showContactInfo ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'contact')/*contact is arg to be passed*/}
+          >
+            Contact
+          </button>
+        </section>
+        <main>{mainContent}</main>
       </aside>
     );
   }
