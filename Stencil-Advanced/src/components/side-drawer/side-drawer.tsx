a54080b3e00c221019d6d66b8da4531001f3affc
Ryan <rrmhearts@gmail.com>
2020-04-09 09:57:50 -0400

    Side drawer CSS. Intro to passing attributes, props.

--- Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
+++ Stencil-Advanced/src/components/side-drawer/side-drawer.tsx
@@ -1,14 +1,24 @@
-import { Component } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
 
 @Component({
-  tag: 'uc-side-drawer'
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css', // Automatically applied!
+  shadow: true // Scope css to this component's shadow dom.
 })
 export class SideDrawer {
+  // Prop watches title, if changes, calls render.
+  // reflectToAttr may have changed to reflect in future Stencil releases.
+  // ^-- updates attribute of html tag. In outside code wants to interact with attribute.
+  @Prop({reflectToAttr: true}) title: string;
+
   render() {
     return (
-      <div>
-        <h1>The Side Drawer</h1>
-      </div>
+      <aside>
+        <header><h1>{this.title}</h1></header>
+        <main>
+          <slot />
+        </main>
+      </aside>
     );
   }
 }
