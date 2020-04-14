352dab3372307c5c742a44836c28cfc47cd83bb4
Ryan <rrmhearts@gmail.com>
2020-04-14 11:59:08 -0400

    Two way binding, Error handling.

--- Stencil-Advanced/src/components/stock-price/stock-price.tsx
+++ Stencil-Advanced/src/components/stock-price/stock-price.tsx
@@ -10,38 +10,76 @@ import { AV_API_KEY } from '../../global/global';
 export class StockPrice {
   stockInput: HTMLInputElement;
 
-  @Element() el: HTMLElement; // HOST Element in light dom.
+  @Element() el: HTMLElement;
 
   @State() fetchedPrice: number;
 
+  /* Two way binding for error handling */
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
+  @State() error: string;
+
+  onUserInput(event: Event) {
+    // Set variable to actual user input.
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+
+    // Do all error checking!
+    if (this.stockUserInput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
+  }
+
   onFetchStockPrice(event: Event) {
     event.preventDefault();
-    // This line is replaced by reference in form.
     // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
     const stockSymbol = this.stockInput.value;
     fetch(
       `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
     )
       .then(res => {
+        if (res.status !== 200) {
+          throw new Error('Invalid!');
+        } // check for 400 errors... throw new Error
         return res.json();
       })
       .then(parsedRes => {
+        // Error checking with this particular api.
+        if (!parsedRes['Global Quote']) {
+          throw new Error('Invalid symbol!');
+        } else if (parsedRes['Error Message']) {
+          throw new Error(parsedRes['Error Message']);
+        }
+
+        this.error = null;
         this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
       })
       .catch(err => {
-        console.log(err);
+        this.error = err.message;
       });
   }
 
   render() {
+    let dataContent = <p>Please enter a symbol!</p>;
+    if (this.error) {
+      dataContent = <p>{this.error}</p>;
+    } else if (this.fetchedPrice) {
+      dataContent = <p>Price: ${this.fetchedPrice}</p>;
+    }
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol" ref={/*stencil trick*/ el => (this.stockInput = el)} />
-        <button type="submit">Fetch</button>
+        <input
+          id="stock-symbol"
+          ref={el => (this.stockInput = el)}
+          value={this.stockUserInput /*binding*/}
+          onInput={this.onUserInput.bind(this)/*update value in var*/}
+        />
+        <button type="submit" disabled={!this.stockInputValid}>
+          Fetch
+        </button>
       </form>,
-      <div>
-        <p>Price: ${this.fetchedPrice}</p>
-      </div>
+      <div>{dataContent}</div>
     ];
   }
 }
